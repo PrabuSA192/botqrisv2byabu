7515e9585302139e3b9ea8f8dc84c3db544227e4
Prabu Setyo A <prabusetyo57@gmail.com>
2026-01-30 22:01:06 +0700

    Clear sensitive data in settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,8 @@
 const settings = {
-    TOKEN : '8337735658:AAHyZU71AFe_WcXNCco7YM53OelUb6GvI8Y',
-    OWNER_ID : '6250933363',
-    ADMIN_LINK : 'https://t.me/abuzycreative',
-    CHANNEL_LINK : 'https://t.me/abuzytesti'
+    TOKEN : '',
+    OWNER_ID : '',
+    ADMIN_LINK : '',
+    CHANNEL_LINK : ''
 };
+
 module.exports = settings;
